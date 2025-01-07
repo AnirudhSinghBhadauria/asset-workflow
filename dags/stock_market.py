d3d8dd6c0c4d4d8aacd9b14dd580b2ebfca33583
Anirudh Singh Bhadauria <108342411+AnirudhSinghBhadauria@users.noreply.github.com>
2025-01-07 23:34:47 +0530

    added the stock price task

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -1,14 +1,14 @@
-from airflow.decorators import dag, task
-from airflow.operators.python import PythonOperator
+import requests
 from datetime import datetime
-from airflow.sensors.base import PokeReturnValue
 from airflow.hooks.base import BaseHook
+from airflow.decorators import dag, task
+from airflow.sensors.base import PokeReturnValue
+from airflow.operators.python import PythonOperator
+from include.stock_market.tasks import (
+     _get_stock_prices
+)
 
-def _print_stock_name():
-     print("this is the stock name")
-     
-     price = 56
-     return price
+SYMBOL = 'AAPL'
 
 @dag(
      start_date=datetime(25, 1, 1),
@@ -20,12 +20,36 @@ def _print_stock_name():
 def stock_market():
      # First task will be to check if the api is available or not using a sensor!
      @task.sensor(
+          task_id='is_api_available',
           poke_interval = 40,
           timeout = 300,
           mode = 'poke'
      )
      def is_api_available() -> PokeReturnValue:
-          # We create this "stock_api" connection in metadatabase of the airflow,
           api = BaseHook.get_connection("stock_api")
+          url = f"{api.host}{api.extra_dejson['endpoint']}"
+          headers = api.extra_dejson['headers']
+          response = requests.get(url, headers=headers)
+          
+          condition = response.json()['finance']['result'] is None
+          
+          return PokeReturnValue(
+               is_done=condition,
+               xcom_value=url
+          )
+     
+     sensor_task = is_api_available()
+     
+     get_stock_prices = PythonOperator(
+          task_id = 'get_stock_prices',
+          python_callable = _get_stock_prices,
+          op_kwargs = {
+               # at runtime this {{..}} will be replace with "https://query1.finance.yahoo.com/"
+               'url': '{{ task_instance.xcom_pull(task_ids="is_api_available") }}',
+               'symbol': SYMBOL
+          }
+     )
+               
+     sensor_task >> get_stock_prices
                
 stock_market()
